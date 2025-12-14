5677948d70ee93b2bf3b73d40b0ec9da54b9f0d5
Frost122 <99787333+Frost122@users.noreply.github.com>
2025-12-14 18:01:41 +0300

    v1.3.5

--- scripts/update-firestore-version.cjs
+++ scripts/update-firestore-version.cjs
@@ -9,7 +9,6 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore();
 
 async function updateVersion() {

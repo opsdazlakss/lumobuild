81095965a694abe29ce05fff9cf007f679307253
Frost122 <99787333+Frost122@users.noreply.github.com>
2025-12-17 22:21:40 +0300

    v1.7.5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.hasan.lumo',
+  appId: 'com.hasan.lumo.mobile',
   appName: 'Lumo',
   webDir: 'dist',
   plugins: {

f8e68e86bea90f21c254bda010d824a4b68bcbe4
Frost122 <99787333+Frost122@users.noreply.github.com>
2025-12-17 20:58:53 +0300

    v1.7.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,6 +13,9 @@ const config: CapacitorConfig = {
       splashFullScreen: true,
       splashImmersive: true,
     },
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
   },
 };
 
